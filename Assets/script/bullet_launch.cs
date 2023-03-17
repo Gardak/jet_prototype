13cfa83780bdde4f698913b2e89933fec0796edb
Gardak <a.lorrain@hotmail.com>
2023-03-17 10:15:48 -0400

    Initial commit

---
+++ Assets/script/bullet_launch.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_launch : MonoBehaviour
+{
+    public GameObject projectile;
+    public float launchVelocity = 700f;
+    public AudioSource Bang;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject ball = Instantiate(projectile, transform.position, transform.rotation);
+            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity, 0));
+            Bang.Play();
+        }
+    }
+}
